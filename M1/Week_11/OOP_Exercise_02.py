ec9125761c320da191efe738ab61052a8b2e0f29
Marcelo <am.erantiel@outlook.com>
2025-06-18 15:39:55 -0600

    Updates

--- M1/Week_11/OOP_Exercise_02.py
+++ M1/Week_11/OOP_Exercise_02.py
@@ -3,6 +3,14 @@ class Person():
         self.name = name
 
 
+    def __str__(self):
+        return self.name
+
+
+    def __repr__(self):
+        return self.name
+
+
 class Bus():
     def __init__(self):
         self.max_passengers = 1
@@ -12,11 +20,15 @@ class Bus():
         if len(passengers) >= self.max_passengers:
             print('The bus is full.')
         else:
-            return passengers
+            return person
 
 passengers = []
 person_01 = Person('Luis')
 person_02 = Person('Marco')
+person_03 = Person('Karen')
 bus_01 = Bus()
 passengers.append(bus_01.add_passenger(passengers,person_01))
 passengers.append(bus_01.add_passenger(passengers,person_02))
+passengers.append(bus_01.add_passenger(passengers,person_03))
+
+print(passengers)
---
+++ M1/Week_11/OOP_Exercise_03/actions.py
@@ -0,0 +1,87 @@
+def ask_for_grade(topic):
+    grade = 0
+    loop = True
+    while loop == True:
+        try:
+            grade = int(input(f'Type the grade obtained in {topic}: '))
+            if grade < 0 or grade > 100:
+                raise ValueError()
+            return grade
+        except ValueError as e:
+            print('\nPlease enter a valid grade between 0 to 100.\n')
+
+
+def ask_for_name():
+    loop = True
+    counter = 0
+    while loop == True:
+        name = input("\nType the student's name: ")
+        for char in name:
+            if char.isalpha() or char.isspace():
+                if counter == len(name)-1:
+                    return name
+                else:
+                    counter += 1
+                    continue
+            else:
+                print('\nPlease, enter a valid name for a student.')
+                counter = 0
+
+
+def ask_for_classroom():
+    loop = True
+    while loop == True:
+        classroom = input('Type their classroom: ')
+        number = classroom[0]
+        letter = classroom[1]
+
+        if len(classroom) > 2:
+            print('\nPlease, enter a valid classroom: first a number followed by a letter.\n')
+        elif number.isdigit():
+            if letter.isalpha():
+                return classroom
+            else:
+                print('\nPlease, enter a valid classroom: first a number followed by a letter.\n')
+        else:
+            print('\nPlease, enter a valid classroom: first a number followed by a letter.\n')
+
+
+
+def add_student():
+    student = {
+        'Name':ask_for_name(),
+        'Classroom':ask_for_classroom(),
+        'Spanish Grade':ask_for_grade('Spanish'),
+        'English Grade':ask_for_grade('English'),
+        'Social Studies Grade':ask_for_grade('Social Studies'),
+        'Science Grade':ask_for_grade('Science')
+    }
+    return student
+
+
+def average_note_every_student(list):
+    list_of_students = list
+    list_of_average_notes = []
+    counter = 0
+
+    for student in list_of_students:
+        list_of_average_notes.append({'Name':student['Name'],'Average Grade':(int(student['Spanish Grade'])+int(student['English Grade'])+int(student['Social Studies Grade'])+int(student['Science Grade']))/4})
+    sorted_list = sorted(list_of_average_notes, key=lambda item:item['Average Grade'], reverse=True)
+    print('\nThe top 3 students are:')
+    for value in sorted_list:
+        if counter < 3:
+            print(value)
+            counter += 1
+
+
+def average_note_all_students(list):
+    list_of_students = list
+    total_average_note = 0
+    total_grades_number = 0
+
+    for student in list_of_students:
+        total_average_note = total_average_note + (int(student['Spanish Grade'])+int(student['English Grade'])+int(student['Social Studies Grade'])+int(student['Science Grade']))
+        total_grades_number += 4
+    
+    total_average_note = total_average_note / total_grades_number
+    print(f'\nThe average value of all students grades is: {total_average_note}')
