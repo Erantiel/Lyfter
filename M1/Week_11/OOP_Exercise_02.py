a6b20b0c299e2ab271b6d2903753206f7757e814
Marcelo <am.erantiel@outlook.com>
2025-06-17 19:55:51 -0600

    Week 11 Exercises

---
+++ M1/Week_11/OOP_Exercise_02.py
@@ -0,0 +1,26 @@
+class Person():
+    def __init__(self,name):
+        self.name = name
+
+
+class Bus():
+    def __init__(self):
+        self.max_passengers = 1
+
+
+    def add_passenger(self,passengers,person):
+        if len(passengers) >= self.max_passengers:
+            print('The bus is full.')
+        else:
+            return passengers
+
+passengers = []
+person_01 = Person('Luis')
+person_02 = Person('Marco')
+bus_01 = Bus()
+passengers.append(bus_01.add_passenger(passengers,person_01))
+passengers.append(bus_01.add_passenger(passengers,person_02))
+
+print(passengers)
+# for index in range(0,len(passengers)):
+#     print(passengers[index].name)
