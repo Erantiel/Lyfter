b4cd91eac2159cc7075e347aa50d69ac7aa6e518
Marcelo <am.erantiel@outlook.com>
2025-06-24 06:00:33 -0600

    Fixing second exercise

--- M1/Week_12/Pillars_Of_OOP_Exercise_02.py
+++ M1/Week_12/Pillars_Of_OOP_Exercise_02.py
@@ -12,61 +12,67 @@ class Shape(ABC):
 
 
 class Circle(Shape):
-    def calculate_perimeter(self, diameter):
+    def __init__(self, diameter, radius):
         self.diameter = diameter
+        self.radius = radius
+
+
+    def calculate_perimeter(self):
         self.perimeter = 3.14 * self.diameter
         return f'The perimeter of the circle with diameter {self.diameter} is {self.perimeter}\n'
 
 
-    def calculate_area(self, radius):
-        self.radius = radius
+    def calculate_area(self):
         self.area = 3.14 * pow(self.radius,2)
         return f'The area of the circle with radius {self.radius} is {self.area}\n'
 
 
 class Square(Shape):
-    def calculate_perimeter(self, side_perimeter):
-        self.side_perimeter = side_perimeter
-        self.perimeter = 4 * self.side_perimeter
-        return f'The perimeter of the square with sides equal to {self.side_perimeter} is {self.perimeter}\n'
+    def __init__(self, side):
+        self.side = side
 
 
-    def calculate_area(self, side_area):
-        self.side_area = side_area
-        self.area = self.side_area * self.side_area
-        return f'The area of the square with sides equal to {self.side_area} is {self.area}\n'
+    def calculate_perimeter(self):
+        self.perimeter = 4 * self.side
+        return f'The perimeter of the square with sides equal to {self.side} is {self.perimeter}\n'
+
+
+    def calculate_area(self):
+        self.area = self.side * self.side
+        return f'The area of the square with sides equal to {self.side} is {self.area}\n'
 
 
 class Rectangle(Shape):
-    def calculate_perimeter(self, length_perimeter, width_perimeter):
-        self.length_perimeter = length_perimeter
-        self.width_perimeter = width_perimeter
-        if self.length_perimeter != self.width_perimeter:
-            self.perimeter = 2 * (self.length_perimeter + self.width_perimeter)
-            return f'The perimeter of the rectangle with length {self.length_perimeter} and width {self.width_perimeter} is {self.perimeter}\n'
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+
+    def calculate_perimeter(self):
+        if self.length != self.width:
+            self.perimeter = 2 * (self.length + self.width)
+            return f'The perimeter of the rectangle with length {self.length} and width {self.width} is {self.perimeter}\n'
         else:
             return f'The length and width of a rectangle must be different in order to calculate the perimeter.\n'
 
 
-    def calculate_area(self, length_area, width_area):
-        self.length_area = length_area
-        self.width_area = width_area
-        if self.length_area != self.width_area:
-            self.area = self.length_area * self.width_area
-            return f'The area of the rectangle with length {self.length_area} and width {self.width_area} is {self.area}\n'
+    def calculate_area(self):
+        if self.length != self.width:
+            self.area = self.length * self.width
+            return f'The area of the rectangle with length {self.length} and width {self.width} is {self.area}\n'
         else:
             return f'The length and width of a rectangle must be different in order to calculate the area.\n'
 
-circle = Circle()
-square = Square()
-rectangle = Rectangle()
+circle = Circle(2,4)
+square = Square(6)
+rectangle = Rectangle(4,8)
 
-print(circle.calculate_perimeter(2))
-print(circle.calculate_area(2))
+print(circle.calculate_perimeter())
+print(circle.calculate_area())
 
-print(square.calculate_perimeter(4))
-print(square.calculate_area(4))
+print(square.calculate_perimeter())
+print(square.calculate_area())
 
-print(rectangle.calculate_perimeter(2,2))
-print(rectangle.calculate_perimeter(2,4))
-print(rectangle.calculate_area(2,4))
+print(rectangle.calculate_perimeter())
+print(rectangle.calculate_perimeter())
+print(rectangle.calculate_area())
