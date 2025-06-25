b660cc886dea0d23bdb3a2f0a4d154b2704f5f52
Marcelo <am.erantiel@outlook.com>
2025-06-25 13:59:39 -0600

    Adding more updates to the logic of check_numeric

--- M1/Week_13/Decorator_Exercise_02.py
+++ M1/Week_13/Decorator_Exercise_02.py
@@ -1,21 +1,32 @@
 def check_numeric(func):
     def wrapper(*args):
         result = func(*args)
-        counter = 0
         for index, arg in enumerate(args):
             try:
                 float(arg)
-                result.append({f'Arg {counter}':f'Number {arg}'})
-                counter += 1
             except ValueError:
                 print(f'The input <{arg}> is not a number.')
+                try:
+                    result.pop(index)
+                except AttributeError:
+                    pass
         return result
     return wrapper
 
 @check_numeric
 def arguments(*args):
     list_of_arg = []
+    for index, arg in enumerate(args):
+        list_of_arg.append(arg)
     return list_of_arg
+
+@check_numeric
+def no_return_list(*args):
+    for index, arg in enumerate(args):
+        return arg
 
 result = arguments(2,'H','4')
 print(result)
+
+no_list = no_return_list('K')
+print(no_list)
