4eceeda4c790e339a274f8e8b22031058dcb5c4e
Marcelo <am.erantiel@outlook.com>
2025-06-26 16:30:26 -0600

    Uploading Week 14 Exercises

---
+++ M1/Week_14/Data_Structure_Exercise_02.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class DoubleEndedQueue:
+    def __init__(self, head):
+        self.head = head
+
+
+    def print_structure(self):
+        current_node = self.head
+
+        while (current_node is not None):
+            print(current_node.data)
+            current_node = current_node.next
+
+
+    def push_right(self, node):
+        current_node = self.head
+
+        while (current_node.next is not None):
+            current_node = current_node.next
+        
+        current_node.next = node
+
+
+    def push_left(self, node):
+        tail = self.head
+        self.head = node
+        self.head.next = tail
+
+
+    def pop_right(self):
+        current_node = self.head
+
+        while (current_node is not None):
+            top = current_node
+            current_node = current_node.next
+        
+        current_node = self.head
+        while (current_node.next != top):
+            current_node = current_node.next
+        
+        current_node.next = None
+
+
+    def pop_left(self):
+        if self.head:
+            self.head = self.head.next
+
+
+forth_node = Node("Forth node")
+third_node = Node("Third node")
+second_node = Node("Second node")
+first_node = Node("First node")
+negative_01 = Node("-1")
+negative_02 = Node("-2")
+
+structure = DoubleEndedQueue(first_node)
+print('---Adding to the right---')
+structure.push_right(second_node)
+structure.push_right(third_node)
+structure.push_right(forth_node)
+structure.print_structure()
+print('---Adding to the left---')
+structure.push_left(negative_01)
+structure.push_left(negative_02)
+structure.print_structure()
+print('---Deleting from right---')
+structure.pop_right()
+structure.print_structure()
+print('---Deleting from left---')
+structure.pop_left()
+structure.print_structure()
