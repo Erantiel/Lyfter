91db19ba63c659ca793c58b8f62b3adfc585ebde
Marcelo <am.erantiel@outlook.com>
2026-05-06 16:06:14 -0600

    Fixing code as per request May 5, 2026 9:47 AM

--- M2_BE/Week_05/main.py
+++ M2_BE/Week_05/main.py
@@ -12,21 +12,23 @@ app = Flask(__name__)
 class Users(MethodView):
     def get(self):
         try:
-            keyword = request.args.get("keyword")
-            allowed_keywords = ["id", "name", "email", "username", "password", "birthday", "status"]
-            if not keyword:
-                query = database.execute_query(f"""
+            filter = request.args.get("filter")
+            value = request.args.get("value")
+            allowed_filters = ["id", "name", "email", "username", "password", "birthday", "status"]
+            if not filter:
+                get_query = database.execute_query("""
                 SELECT * FROM lyfter_car_rental.users
                 ORDER BY id ASC
                 """)
-                return jsonify({"response":query}), 200
-            if keyword not in allowed_keywords:
+                return jsonify({"response":get_query}), 200
+            if filter not in allowed_filters:
                 return jsonify({"error":"This method only accepts id, name, email, username, password, birthday or status."}), 404
-            query = database.execute_query(f"""
+            filter_query = database.execute_query(f"""
             SELECT * FROM lyfter_car_rental.users
-            ORDER BY {keyword} ASC
-            """)
-            return jsonify({"response":query}), 200
+            WHERE {filter} = %s
+            """,(value,))
+            database.close_connection
+            return jsonify({"response":filter_query}), 200
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
 
@@ -39,14 +41,9 @@ class Users(MethodView):
             validations.validate_data_users(data)
             database.execute_query("""
             INSERT INTO lyfter_car_rental.users(name, email, username, password, birthday)
-            VALUES (%s, %s, %s, %s, %s)""",
-            (
-                data['name'],
-                data['email'],
-                data['username'],
-                data['password'],
-                data['birthday']
-            ))
+            VALUES (%s, %s, %s, %s, %s)
+            """,(data['name'], data['email'], data['username'], data['password'], data['birthday']))
+            database.close_connection
             return jsonify({"response":"User added."}), 201
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
@@ -64,12 +61,12 @@ class Users(MethodView):
                 return jsonify({"error":"The filter only accepts id."}), 404
             if update != "status":
                 return jsonify({"error":"You can only update the status of a user."}), 404
-            query = f"""
+            database.execute_query(f"""
             UPDATE lyfter_car_rental.users 
             SET {update} = %s
             WHERE {filter} = %s
-            """
-            database.execute_query(query, (data[update], value))
+            """,(data[update], value))
+            database.close_connection
             return jsonify({"response":"Database updated."}), 200
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
@@ -78,21 +75,23 @@ class Users(MethodView):
 class Vehicles(MethodView):
     def get(self):
         try:
-            keyword = request.args.get("keyword")
-            allowed_keywords = ["id", "make", "model", "manufacture_year", "status"]
-            if not keyword:
-                query = database.execute_query(f"""
+            filter = request.args.get("filter")
+            value = request.args.get("value")
+            allowed_filters = ["id", "make", "model", "manufacture_year", "status"]
+            if not filter:
+                get_query = database.execute_query(f"""
                 SELECT * FROM lyfter_car_rental.vehicles
                 ORDER BY id ASC
                 """)
-                return jsonify({"response":query}), 200
-            if keyword not in allowed_keywords:
+                return jsonify({"response":get_query}), 200
+            if filter not in allowed_filters:
                 return jsonify({"error":"This method only accepts id, make, model, manufacture_year or status."}), 404
-            query = database.execute_query(f"""
+            filter_query = database.execute_query(f"""
             SELECT * FROM lyfter_car_rental.vehicles
-            ORDER BY {keyword} ASC
-            """)
-            return jsonify({"response":query}), 200
+            WHERE {filter} = %s
+            """,(value,))
+            database.close_connection
+            return jsonify({"response":filter_query}), 200
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
 
@@ -105,12 +104,8 @@ class Vehicles(MethodView):
             validations.validate_data_vehicles(data)
             database.execute_query("""
             INSERT INTO lyfter_car_rental.vehicles(make, model, manufacture_year)
-            VALUES (%s, %s, %s)""",
-            (
-                data['make'],
-                data['model'],
-                data['manufacture_year']
-            ))
+            VALUES (%s, %s, %s)""",(data['make'], data['model'], data['manufacture_year']))
+            database.close_connection
             return jsonify({"response":"Vehicle added."}), 201
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
@@ -128,34 +123,37 @@ class Vehicles(MethodView):
                 return jsonify({"error":"The filter only accepts id."}), 404
             if update != "status":
                 return jsonify({"error":"You can only update the status of a vehicle."}), 404
-            query = f"""
+            database.execute_query(f"""
             UPDATE lyfter_car_rental.vehicles 
             SET {update} = %s
             WHERE {filter} = %s
-            """
-            database.execute_query(query, (data[update], value))
+            """,(data[update], value))
+            database.close_connection
             return jsonify({"response":"Database updated."}), 200
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
 
+
 class UsersVehicles(MethodView):
     def get(self):
         try:
-            keyword = request.args.get("keyword")
-            allowed_keywords = ["user_id", "vehicle_id"]
-            if not keyword:
-                query = database.execute_query(f"""
+            filter = request.args.get("filter")
+            value = request.args.get("value")
+            allowed_filters = ["user_id", "vehicle_id"]
+            if not filter:
+                get_query = database.execute_query(f"""
                 SELECT * FROM lyfter_car_rental.users_vehicles
                 ORDER BY id ASC
                 """)
-                return jsonify({"response":query}), 200
-            if keyword not in allowed_keywords:
+                return jsonify({"response":get_query}), 200
+            if filter not in allowed_filters:
                 return jsonify({"error":"This method only accepts user_id and vehicle id."}), 404
-            query = database.execute_query(f"""
+            filter_query = database.execute_query(f"""
             SELECT * FROM lyfter_car_rental.users_vehicles
-            ORDER BY {keyword} ASC
-            """)
-            return jsonify({"response":query}), 200
+            WHERE {filter} = &s
+            """,(value,))
+            database.close_connection
+            return jsonify({"response":filter_query}), 200
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
 
@@ -166,19 +164,26 @@ class UsersVehicles(MethodView):
             data = request.json
             validations.validate_keys_users_vehicles(data)
             validations.validate_data_users_vehicles(data)
+            car_status_query = database.execute_query(f"""
+            SELECT status FROM lyfter_car_rental.vehicles
+            WHERE id = %s
+            """,(data['vehicle_id']))
+            car_status = car_status_query[0][0]
+            print(car_status)
+            if car_status == 'rented':
+                return jsonify({"error":"The vehicle you are trying to rent is already rented by an user."}), 400
+            elif car_status == 'not available':
+                return jsonify({"error":"The vehicle you are trying to rent is not available."}), 400
             database.execute_query("""
             INSERT INTO lyfter_car_rental.users_vehicles(user_id, vehicle_id)
-            VALUES (%s, %s)""",
-            (
-                data['user_id'],
-                data['vehicle_id']
-            ))
+            VALUES (%s, %s)""",(data['user_id'], data['vehicle_id']))
             database.execute_query(f"""
             UPDATE lyfter_car_rental.vehicles
             SET status = 'rented'
             WHERE id = %s
             """,
             (data['vehicle_id']))
+            database.close_connection
             return jsonify({"response":"Rent added."}), 201
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
@@ -216,6 +221,7 @@ class UsersVehicles(MethodView):
             database.execute_query(query_update_status, (data[update], value))
             database.execute_query(query_rent_completed, (value))
             database.execute_query(query_date_completion, (value))
+            database.close_connection
             return jsonify({"response":"Database updated."}), 200
         except ValueError as ex:
             return jsonify({"error":str(ex)}), 400
