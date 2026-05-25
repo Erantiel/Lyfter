5c7408e5c44455ba550dd8b0fd90e70654b23ed1
Marcelo <am.erantiel@outlook.com>
2026-05-25 12:15:17 -0600

    Uplaoding changes requested by Andre Bonilla May 23,  2026 at 8:26:17 AM

--- M2_BE/ECommerce_Project/filler_queries.sql
+++ M2_BE/ECommerce_Project/filler_queries.sql
@@ -33,16 +33,16 @@ INSERT INTO e_commerce.user (name, username, password, email, role_id) VALUES ('
 INSERT INTO e_commerce.user (name, username, password, email, role_id) VALUES ('Milicent Dommersen', 'mdommersen8', 'eM0\4`KHYLb', 'mdommersen8@sourceforge.net', 2);
 INSERT INTO e_commerce.user (name, username, password, email, role_id) VALUES ('Amalee Gilfether', 'agilfether9', 'yJ9~ZME!*', 'agilfether9@ftc.gov', 2);
 --INSERT of Product
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('043310980', 'Dog Dry Food', '$20', '1KG', 1, 3);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('031100490', 'Dog Treats', '$7', '150G', 1, 1);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('081006052', 'Cat Dry Food', '$18', '1KG', 1, 2);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('042104469', 'Cat Treats', '$7', '150G', 1, 2);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('092900956', 'Dog Harness', '$15', 'Adjustable', 2, 4);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('081501175', 'Cat Harness', '$12', 'Adjustable', 2, 5);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('031275998', 'Bone Plush', '$15', 'Wool Made', 3, 1);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('061192753', 'Fish Plush', '$10', 'Wool Made', 3, 3);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('051408923', 'Bowl', '$20', 'Stainless Steel', 4, 4);
-INSERT INTO e_commerce.product (sku, name, price, description, category_id, brand_id) VALUES ('111906161', 'Scratching Post', '$35', 'Non Toxic Sisal', 4, 5);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('043310980', 'Dog Dry Food', 20, 'Dollar', '1KG', 1, 3);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('031100490', 'Dog Treats', 7, 'Dollar', '150G', 1, 1);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('081006052', 'Cat Dry Food', 18, 'Dollar', '1KG', 1, 2);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('042104469', 'Cat Treats', 7, 'Dollar', '150G', 1, 2);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('092900956', 'Dog Harness', 15, 'Dollar', 'Adjustable', 2, 4);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('081501175', 'Cat Harness', 12, 'Dollar', 'Adjustable', 2, 5);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('031275998', 'Bone Plush', 15, 'Dollar', 'Wool Made', 3, 1);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('061192753', 'Fish Plush', 10, 'Dollar', 'Wool Made', 3, 3);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('051408923', 'Bowl', 20, 'Dollar', 'Stainless Steel', 4, 4);
+INSERT INTO e_commerce.product (sku, name, price, currency, description, category_id, brand_id) VALUES ('111906161', 'Scratching Post', 35, 'Dollar', 'Non Toxic Sisal', 4, 5);
 --INSERT of Store
 INSERT INTO e_commerce.store (product_id, amount, expiration_date, status_id) VALUES (1, 30, '2026-7-19', 1);
 INSERT INTO e_commerce.store (product_id, amount, status_id) VALUES (2, 0, 2);
@@ -55,17 +55,19 @@ INSERT INTO e_commerce.store (product_id, amount, status_id) VALUES (8, 3, 4);
 INSERT INTO e_commerce.store (product_id, amount, status_id) VALUES (9, 13, 1);
 INSERT INTO e_commerce.store (product_id, amount, status_id) VALUES (10, 6, 1);
 --INSERT of Bill
-INSERT INTO e_commerce.bill (user_id, total_amount) VALUES (2, '$67');
-INSERT INTO e_commerce.bill (user_id, total_amount) VALUES (2, '$85');
-INSERT INTO e_commerce.bill (user_id, total_amount) VALUES (7, '$100');
+INSERT INTO e_commerce.bill (user_id, total_amount, currency, payment_method_id) VALUES (2, 67, 'Dollar', 2);
+INSERT INTO e_commerce.bill (user_id, total_amount, currency, payment_method_id) VALUES (2, 85, 'Dollar', 2);
+INSERT INTO e_commerce.bill (user_id, total_amount, currency, payment_method_id) VALUES (7, 100, 'Dollar', 2);
 --INSERT of Product_Bill
-INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount) VALUES (1, 1, 3, '$60');
-INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount) VALUES (4, 1, 1, '$7');
-INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount) VALUES (5, 2, 1, '$15');
-INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount) VALUES (10, 2, 2, '$70');
-INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount) VALUES (9, 3, 2, '$40');
-INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount) VALUES (7, 3, 4, '$60');
+INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount, currency) VALUES (1, 1, 3, 60, 'Dollar');
+INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount, currency) VALUES (4, 1, 1, 7, 'Dollar');
+INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount, currency) VALUES (5, 2, 1, 15, 'Dollar');
+INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount, currency) VALUES (10, 2, 2, 70, 'Dollar');
+INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount, currency) VALUES (9, 3, 2, 40, 'Dollar');
+INSERT INTO e_commerce.product_bill (product_id, bill_id, product_amount, total_amount, currency) VALUES (7, 3, 4, 60, 'Dollar');
 --INSERT of Shopping Cart
-INSERT INTO e_commerce.shopping_cart (id, user_id, product_id, product_amount, payment_method_id) VALUES (1, 7, 1, 2, 2);
-INSERT INTO e_commerce.shopping_cart (id, user_id, product_id, product_amount, payment_method_id) VALUES (1, 7, 4, 1, 2);
-INSERT INTO e_commerce.shopping_cart (id, user_id, product_id, product_amount, payment_method_id) VALUES (1, 8, 1, 5, 2);
+INSERT INTO e_commerce.shopping_cart (user_id) VALUES (7);
+INSERT INTO e_commerce.shopping_cart (user_id) VALUES (8);
+--INSERT of Product Shopping Cart
+INSERT INTO e_commerce.product_shopping_cart (shopping_cart_id, product_id, product_amount) VALUES (1, 1, 10);
+INSERT INTO e_commerce.product_shopping_cart (shopping_cart_id, product_id, product_amount) VALUES (2, 10, 1);
--- M2_BE/ECommerce_Project/table_queries.sql
+++ M2_BE/ECommerce_Project/table_queries.sql
@@ -44,7 +44,8 @@ CREATE TABLE e_commerce.product(
         id SERIAL PRIMARY KEY,
         sku VARCHAR(100) UNIQUE NOT NULL,
         name VARCHAR(100) NOT NULL,
-        price VARCHAR(100) NOT NULL,
+        price INT NOT NULL,
+        currency VARCHAR(100) NOT NULL,
         description TEXT NOT NULL,
         category_id INT NOT NULL,
         FOREIGN KEY (category_id) REFERENCES e_commerce.category(id),
@@ -70,7 +71,10 @@ CREATE TABLE e_commerce.bill(
         user_id INT NOT NULL,
         FOREIGN KEY (user_id) REFERENCES e_commerce.user(id),
         date_of_purchase DATE DEFAULT CURRENT_DATE,
-        total_amount VARCHAR(100) NOT NULL
+        total_amount INT NOT NULL,
+        currency VARCHAR(100) NOT NULL,
+        payment_method_id INT NOT NULL,
+        FOREIGN KEY (payment_method_id) REFERENCES e_commerce.payment_method(id)
         );
 
 
@@ -81,17 +85,23 @@ CREATE TABLE e_commerce.product_bill(
         bill_id INT NOT NULL,
         FOREIGN KEY (bill_id) REFERENCES e_commerce.bill(id),
         product_amount INT NOT NULL,
-        total_amount VARCHAR(100) NOT NULL
+        total_amount INT NOT NULL,
+        currency VARCHAR(100)
         );
 
 
 CREATE TABLE e_commerce.shopping_cart(
         id SERIAL PRIMARY KEY,
         user_id INT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES e_commerce.user(id),
+        FOREIGN KEY (user_id) REFERENCES e_commerce.user(id)
+        );
+
+
+CREATE TABLE e_commerce.product_shopping_cart(
+        id SERIAl PRIMARY KEY,
+        shopping_cart_id INT NOT NULL,
+        FOREIGN KEY (shopping_cart_id) REFERENCES e_commerce.shopping_cart(id),
         product_id INT NOT NULL,
         FOREIGN KEY (product_id) REFERENCES e_commerce.product(id),
-        product_amount INT NOT NULL,
-        payment_method_id INT NOT NULL,
-        FOREIGN KEY (payment_method_id) REFERENCES e_commerce.payment_method(id)
-        );
+        product_amount INT NOT NULL
+        )
