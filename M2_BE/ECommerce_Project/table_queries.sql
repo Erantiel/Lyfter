180522c13f562621ed14a672df830d4cc4946dae
Marcelo <am.erantiel@outlook.com>
2026-05-25 12:52:55 -0600

    Password VARCHAR(250)

--- M2_BE/ECommerce_Project/table_queries.sql
+++ M2_BE/ECommerce_Project/table_queries.sql
@@ -33,7 +33,7 @@ CREATE TABLE e_commerce.user(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         username VARCHAR(100) NOT NULL UNIQUE,
-        password VARCHAR(100) NOT NULL,
+        password VARCHAR(250) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
         role_id INT NOT NULL,
         FOREIGN KEY (role_id) REFERENCES e_commerce.role(id)
